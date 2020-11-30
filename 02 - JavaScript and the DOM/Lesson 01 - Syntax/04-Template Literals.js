bb79adf2cf8da61a373beb4664439db84edadae7
anwarvic <mohamedanwarvic@gmail.com>
2020-12-01 01:24:07 +0200

    solved quiz of the lesson 01 of the second part

---
+++ 02 - JavaScript and the DOM/Lesson 01 - Syntax/04-Template Literals.js
@@ -0,0 +1,8 @@
+/*
+ * Instructions: Change the `greeting` string to use a template literal.
+ */
+
+const myName = 'Anwar';
+const greeting = `Hello, my name is ${myName}`;
+console.log(greeting);
+
