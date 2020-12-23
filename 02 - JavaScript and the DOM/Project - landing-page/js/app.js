6dd01a9f2911506355c87b17ca7f0a0e64e9d9a2
Anwar <mohamed.anwar@istnetworks.com>
2020-12-23 03:13:47 +0200

    found a way to scroll to the active section

--- 02 - JavaScript and the DOM/Project - landing-page/js/app.js
+++ 02 - JavaScript and the DOM/Project - landing-page/js/app.js
@@ -19,6 +19,7 @@
 */
 const sections = document.querySelectorAll("section");
 const navList = document.querySelector("#navbar__list");
+let activeSectionID = sections[0].textContent;
 
 
 /**
@@ -41,17 +42,36 @@ const fragment = document.createDocumentFragment();
 for (let i = 0; i < sections.length; i++) {
     const newElement = document.createElement('li');
     const heading = sections[i].querySelector("h2").textContent;
+    // adding the inner text
     newElement.innerText = heading;
+    // adding the class
     newElement.classList.add("menu__link");
+    // adding an onclick event listener
+    newElement.addEventListener('click', function(event){
+        const currentSection = event.target;
+        // Add class 'active' to section when near top of viewport
+        
+        // remove 'active' class from activeSection
+        const activeSection = document.querySelector("#"+activeSectionID);
+        activeSection.classList.remove("active");
+        // add 'active' class to the currentSection
+        currentSection.classList.add("active");
+        activeSection = currentSection;
+
+        // scroll to currentSection
+        currentSection.scrollIntoView();
+        
+        // Scroll to anchor ID using scrollTO event
+        // const boundingRect = currentSection.getBoundingClientRect();
+        // currentSection.scrollTo({
+        //     top: boundingRect.top,
+        //     left: boundingRect.left,
+        //     behavior: 'smooth'
+        // });
+    });
     fragment.appendChild(newElement);
 }
-
 navList.appendChild(fragment);
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
 
 
 /**
