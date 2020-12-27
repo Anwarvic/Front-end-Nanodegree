71b811447cb8561353f90ed5b8863fbea1225c78
anwarvic <mohamedanwarvic@gmail.com>
2020-12-27 22:52:22 +0200

    finished app.js

--- 02 - JavaScript and the DOM/Project - landing-page/js/app.js
+++ 02 - JavaScript and the DOM/Project - landing-page/js/app.js
@@ -19,7 +19,7 @@
 */
 const sections = document.querySelectorAll("section");
 const navList = document.querySelector("#navbar__list");
-let activeSection = sections[0];
+let activeSectionId = sections[0].id;
 
 
 /**
@@ -28,6 +28,21 @@ let activeSection = sections[0];
  * 
 */
 
+function activateSection(currentSection){
+    // remove 'active' class from activeSection
+    const activeSection = document.getElementById(activeSectionId);
+    activeSection.classList.remove("active");
+    // remove `active` from former nav item
+    let navItem = document.getElementById("navitem_"+activeSectionId);
+    navItem.classList.remove("active");
+    
+    // add 'active' class to the currentSection
+    currentSection.classList.add("active");
+    activeSectionId = currentSection.id;
+    // set item in the nav bar as active as well
+    navItem = document.getElementById("navitem_"+activeSectionId);
+    navItem.classList.add("active");
+}
 
 
 /**
@@ -40,21 +55,29 @@ let activeSection = sections[0];
 const fragment = document.createDocumentFragment();
 for (let i = 0; i < sections.length; i++) {
     const newElement = document.createElement('li');
-    // adding the inner text
-    newElement.innerText = sections[i].querySelector("h2").textContent;
-    // adding the class
-    newElement.classList.add("menu__link");
-    // adding an onclick event listener
-    newElement.addEventListener('click', function(){
-        const currentSection = document.getElementById("section"+(i+1));
-        // Add class 'active' to section when near top of viewport
-        
-        // remove 'active' class from activeSection
-        activeSection.classList.remove("active");
-        // add 'active' class to the currentSection
-        currentSection.classList.add("active");
-        activeSection = currentSection;
+    newElement.id = "navitem_section"+(i+1);
 
+    // create <a> element
+    const anchor = document.createElement('a');
+    anchor.href = '#section'+(i+1);
+    
+    // adding the `menu__link` class
+    anchor.classList.add("menu__link");
+    anchor.innerText = sections[i].querySelector("h2").textContent;
+
+    // appending the link to the li element
+    newElement.appendChild(anchor)
+
+    // adding an onclick event listener
+    newElement.addEventListener('click', function(event){
+        event.preventDefault();
+
+        const currentSection = document.getElementById("section"+(i+1));
+        // Set current section as active
+        activateSection(currentSection);
+
+        
+        
         // Scroll to anchor ID using scrollTo event
         const navListHeight = navList.offsetHeight;
         const boundingRect = currentSection.getBoundingClientRect();
@@ -69,6 +92,17 @@ for (let i = 0; i < sections.length; i++) {
 navList.appendChild(fragment);
 
 
+// building the scrolling functionality
+document.addEventListener('scroll', function(){
+    const navListHeight = navList.offsetHeight;
+    sections.forEach(function(currentSection) {
+        // check if the current position is within the section boundaries
+        if(currentSection.offsetTop <= window.scrollY+navListHeight) {
+            // console.log(currentSection.querySelector("h2").textContent);
+            activateSection(currentSection);
+        }
+    });
+});
 /**
  * End Main Functions
  * 
